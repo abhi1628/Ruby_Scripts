84dcee07b60b8c4589d048ddb8c63728b2601845
Abhishek Singh <abhishek.ec@global.org.in>
2022-09-11 12:28:01 +0530

    Create add_2_nums.rb

---
+++ add_2_nums.rb
@@ -0,0 +1,8 @@
+puts 'Enter 1st number:'
+num1 = gets
+puts 'Enter 2nd number:'
+num2 = gets
+res = Integer(num1) + Integer(num2)
+puts 'The result is:', Integer(res)
+sum = num1.to_i + num2.to_i
+puts 'The sum is:', sum.to_i
