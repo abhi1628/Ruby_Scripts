2c6c44a359357a0218f85aead6a2e1829a1b6106
Abhishek Singh <abhishek.ec@global.org.in>
2022-09-11 13:33:37 +0530

    Create string_length.rb

---
+++ string_length.rb
@@ -0,0 +1,7 @@
+# calculating string length with spaces
+mystr = "Hello World"
+print "Length of the sting with spaces is: ",mystr.length
+puts
+# calculating string length without spaces
+mystr = "Hello World".delete(' ')
+print "Length of the sting without spaces is: ",mystr.length
