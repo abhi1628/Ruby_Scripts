2855f3f36af2b85d08b69f82e566cf12ceb87263
Abhishek Singh <abhishek.ec@global.org.in>
2022-09-11 14:55:11 +0530

    Create compare_str_1.rb

---
+++ compare_str_1.rb
@@ -0,0 +1,15 @@
+Str1 = "Hello"
+Str2 = "hello"
+Str3 = "Hello"
+
+if (Str1 == Str2)
+    print "Str1 and Str2 are equal\n"
+else
+    print "Str1 and Str2 are not equal\n"
+end
+
+if (Str1 == Str3)
+    print "Str1 and Str3 are equal\n"
+else
+    print "Str1 and Str3 are not equal\n"
+end
