ce551e17ca36b6bf80620a5ff3a376ceacd73289
Abhishek Singh <abhishek.ec@global.org.in>
2022-09-11 13:18:14 +0530

    Create creating_strings.rb

---
+++ creating_strings.rb
@@ -0,0 +1,19 @@
+MyStr1 = "Hello World"
+MyStr2 = 'Hello India'
+MyStr3 = 'Hello Ruby'
+
+puts MyStr1
+puts MyStr2
+puts MyStr3
+
+# Print a string enclosed in 
+# single quotes
+puts 'Single quote string'
+
+# Print a string enclosed in 
+# double quotes
+puts "Double quote string"
+
+# Print a string enclosed in 
+# triple quotes
+puts '''Triple quote string'''
