9a9f341583b8618216595dec8b7f997ca1d9bb60
Abhishek Singh <abhishek.ec@global.org.in>
2022-09-11 13:50:37 +0530

    Create substring.rb

---
+++ substring.rb
@@ -0,0 +1,7 @@
+mystr = "Hello World"
+substr1 = mystr[2..7]
+substr2 = mystr[2...7]
+
+print "Original string: ",mystr,"\n";
+print "Sub-string including the last value: ",substr1,"\n"
+print "Sub-string excluding the last value: ",substr2,"\n"
