45087c7be75e364143bce00db2829c65f1f7b344
Abhishek Singh <abhishek.ec@global.org.in>
2022-09-11 14:45:45 +0530

    Create concate_strings.rb

---
+++ concate_strings.rb
@@ -0,0 +1,15 @@
+# Concatenate using "+" operator.
+MyStr1 = "Hello " + "World"  
+puts MyStr1
+
+# Concatenate using a single space.  
+MyStr2 = "Hello " "World"  
+puts MyStr2
+  
+# Concatenate using "<<" operator.
+MyStr3 = "Hello " << "World"  
+puts MyStr3
+  
+# Concatenate using concat() method.
+MyStr4 = "Hello ".concat("World")
+puts MyStr4
