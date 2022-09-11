e7dadac190b3b8b34374fa0b57fb209bc428498b
Abhishek Singh <abhishek.ec@global.org.in>
2022-09-11 12:25:22 +0530

    Create displaying_output.rb

---
+++ displaying_output.rb
@@ -0,0 +1,14 @@
+puts 'This is my first program in Ruby'
+puts "Hello World! This is Ruby."
+print 'Hello World! This is Ruby'
+print "Ruby is fun like Python"
+=begin 
+Key Points-
+1. puts and print are used to 
+print the outputs.
+2. Both single and double quotes 
+can be used.
+3. print continues the statement 
+in the same line. puts create new 
+lines.
+=end
