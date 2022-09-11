eb58b0854979796c50c583b6ae0a4b5168818297
Abhishek Singh <abhishek.ec@global.org.in>
2022-09-11 13:23:17 +0530

    Create quotes_difference.rb

---
+++ quotes_difference.rb
@@ -0,0 +1,9 @@
+MyStr1 = "Hello World"
+MyStr2 = "Hello India"
+MyStr3 = '''Hello India'''
+MyStr4 = """Hello India"""
+
+puts 'Single quote string: #{Mystr1}'
+puts "Double quote string: #{MyStr2}"
+puts '''Double quote string: #{MyStr3}'''
+puts """Double quote string: #{MyStr4}"""
