80cc739489528a34c7e814742550e051bd384aaf
Abhishek Singh <abhishek.ec@global.org.in>
2022-09-11 12:23:35 +0530

    Create user_defined_inputs.rb

---
+++ user_defined_inputs.rb
@@ -0,0 +1,6 @@
+puts 'Enter a number:'
+num = gets # gets is used to give userdefined input
+puts 'The number entered is:', num
+puts 'Enter the string:'
+string = gets
+puts 'The string entered is:', string
